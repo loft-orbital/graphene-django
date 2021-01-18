09fb436165331551d13863c4f2c9aee380f028c4
Lucas Brémond <lucas.bremond@gmail.com>
2021-01-17 22:26:51 -0800

    Add native enum filtering support

--- graphene_django/filter/utils.py
+++ graphene_django/filter/utils.py
@@ -5,6 +5,7 @@ import graphene
 from django_filters.utils import get_model_field
 from django_filters.filters import Filter, BaseCSVFilter
 
+from ..settings import graphene_settings
 from .filterset import custom_filterset_factory, setup_filterset
 from .filters import InFilter, RangeFilter
 
@@ -48,6 +49,28 @@ def get_filtering_args_from_filterset(filterset_class, type):
             field = graphene.List(field.get_type())
 
         field_type = field.Argument()
+
+        if graphene_settings.USE_ENUM_TYPE_IN_FILTER:
+
+            filter_field_custom = type._meta.fields.get(filter_field.field_name, None)
+
+            if filter_field_custom is not None:
+
+                filter_field_type = filter_field_custom.type
+
+                if isinstance(filter_field_type, graphene.NonNull):
+                    filter_field_type = filter_field_type.of_type
+
+                if isinstance(filter_field_type, graphene.types.enum.EnumMeta):
+
+                    required = filter_field.extra.get("required", False)
+
+                    if filter_type == "exact":
+                        field_type = filter_field_type(required=required)
+
+                    elif filter_type in {"in", "contains", "overlap"}:
+                        field_type = graphene.List(filter_field_type, required=required)
+
         field_type.description = filter_field.label
         args[name] = field_type
 
