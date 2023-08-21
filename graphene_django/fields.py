594e3e0e3a03f2b867156a28d9f5126e5254b6b9
Laurent Riviere <laurent@loftorbital.com>
2023-08-21 09:27:32 +0000

    fix: import

--- graphene_django/fields.py
+++ graphene_django/fields.py
@@ -4,7 +4,6 @@ from django.db.models.query import QuerySet
 from graphql_relay import (
     connection_from_array_slice,
     cursor_to_offset,
-    get_offset_with_default,
     offset_to_cursor,
 )
 from promise import Promise
