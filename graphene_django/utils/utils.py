d29847277eb63e9543f0ba05f32dd54b4cba000e
Laurent Riviere <laurent@loftorbital.com>
2023-10-20 08:09:09 +0000

    fix: style

--- graphene_django/utils/utils.py
+++ graphene_django/utils/utils.py
@@ -159,7 +159,7 @@ def get_info_cache_key(info):
         else (info.return_type, *map(id, info.field_nodes))
     )
 
-  
+
 _DJANGO_VERSION_AT_LEAST_4_2 = django.VERSION[0] > 4 or (
     django.VERSION[0] >= 4 and django.VERSION[1] >= 2
 )
