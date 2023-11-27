bc4378a11f0576bba26186082c8b9bbb80429d09
Laurent Riviere <laurent@loftorbital.com>
2023-11-27 10:49:25 +0000

    feat: DjangoDataloadedListField

--- graphene_django/fields.py
+++ graphene_django/fields.py
@@ -35,7 +35,63 @@ class DjangoListField(Field):
         if isinstance(_type, NonNull):
             _type = _type.of_type
 
-        # Django would never return a Set of None  vvvvvvv
+        # Django would never return a Set of None
+        super().__init__(List(NonNull(_type)), *args, **kwargs)
+
+        assert issubclass(
+            self._underlying_type, DjangoObjectType
+        ), "DjangoListField only accepts DjangoObjectType types"
+
+    @property
+    def _underlying_type(self):
+        _type = self._type
+        while hasattr(_type, "of_type"):
+            _type = _type.of_type
+        return _type
+
+    @property
+    def model(self):
+        return self._underlying_type._meta.model
+
+    def get_manager(self):
+        return self.model._default_manager
+
+    @staticmethod
+    def list_resolver(
+        django_object_type, resolver, default_manager, root, info, **args
+    ):
+        queryset = maybe_queryset(resolver(root, info, **args))
+        if queryset is None:
+            queryset = maybe_queryset(default_manager)
+
+        if isinstance(queryset, QuerySet):
+            # Pass queryset to the DjangoObjectType get_queryset method
+            queryset = maybe_queryset(django_object_type.get_queryset(queryset, info))
+
+        return queryset
+
+    def wrap_resolve(self, parent_resolver):
+        resolver = super().wrap_resolve(parent_resolver)
+        _type = self.type
+        if isinstance(_type, NonNull):
+            _type = _type.of_type
+        django_object_type = _type.of_type.of_type
+        return partial(
+            self.list_resolver,
+            django_object_type,
+            resolver,
+            self.get_manager(),
+        )
+
+
+class DjangoDataloadedListField(Field):
+    def __init__(self, _type, *args, **kwargs):
+        from .types import DjangoObjectType
+
+        if isinstance(_type, NonNull):
+            _type = _type.of_type
+
+        # Django would never return a Set of None
         super().__init__(List(NonNull(_type)), *args, **kwargs)
 
         assert issubclass(
